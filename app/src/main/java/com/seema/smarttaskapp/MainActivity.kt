a848948cb06fe8f137aa0949ca3111b7d7d3af93
Seema Palora <s.palora87@gmail.com>
2026-02-02 12:46:31 -0500

    Initial Project setup with compose, hilt, mvvm

---
+++ app/src/main/java/com/seema/smarttaskapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.seema.smarttaskapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.seema.smarttaskapp.ui.theme.SmartTaskAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SmartTaskAppTheme {
+                Surface{ Text("SmartTasks App") }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/seema/smarttaskapp/SmartTaskApp.kt
@@ -0,0 +1,8 @@
+package com.seema.smarttaskapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SmartTaskApp:Application() {
+}
